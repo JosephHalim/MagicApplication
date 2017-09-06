b0be5096cf695692c770ddb62a7f93878a3eed80
JosephHalim <joseph.e.halim@gmail.com>
2017-09-05 19:49:09 -0700

    Add packages

---
+++ MagicApp/CardInfo.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+   /* public void LoadJson()
+    {
+        JObject Data = JObject.Parse(File.ReadAllText(@"C:\Users\Joseph\Documents\Visual Studio 2017\Projects\MagicApplication\MagicApp\AllSets.json"));
+       // List<CardInfo> items = JsonConvert.DeserializeObject<List<CardInfo>>(Data);
+        
+    }
+    class CardInfo
+    {
+    }*/
+}
--- MagicApp/MainPage.xaml.cs
+++ MagicApp/MainPage.xaml.cs
@@ -18,6 +18,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,9 +29,14 @@ namespace App1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<Treatment> treatments;
+
+        public object Server { get; private set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+
         }
 
         private void txb_API_SelectionChanged(object sender, RoutedEventArgs e)
@@ -40,20 +46,23 @@ namespace App1
 
 
 
-        private async void Test_ClickAsync()
+        private static void Test_ClickAsync()
         {
-            string data = await callAPIAsync();
-            JObject token = JObject.Parse(data);
-            txb_Name.Text = (string)token.SelectToken("name");
-            txb_cmc.Text = (string)token.SelectToken("cmc");
-            txb_type_line.Text = (string)token.SelectToken("type_line");
-            txb_oracle_text.Text = (string)token.SelectToken("oracle_text");
-            txb_mana_cost.Text = (string)token.SelectToken("mana_cost");
+            /*  // string data = await callAPIAsync();
+              // JObject token = JObject.Parse(data);
+              JObject token;
+              token = JObject.Parse(File.ReadAllText(@"C:\Users\Joseph\Documents\Visual Studio 2017\Projects\MagicApplication\MagicApp\AllSets.json"));
+              txb_Name.Text = (string)token.SelectToken("name");
+              txb_cmc.Text = (string)token.SelectToken("cmc");
+              txb_type_line.Text = (string)token.SelectToken("type_line");
+              txb_oracle_text.Text = (string)token.SelectToken("oracle_text");
+              txb_mana_cost.Text = (string)token.SelectToken("mana_cost");*/
         }
 
         private async System.Threading.Tasks.Task<string> callAPIAsync()
         {
             Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            //Pull data from Json File
             Uri requestUri = new Uri("https://api.scryfall.com/cards/vis/127");
 
             Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
@@ -61,13 +70,39 @@ namespace App1
             httpResponse.EnsureSuccessStatusCode();
             string httpResponseBody = "";
             return httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
-            
+
+        }
+        private void PopulateData(JObject data)
+        {
+            txb_Name.Text = (string)data.SelectToken("name");
+            txb_cmc.Text = (string)data.SelectToken("cmc");
+            txb_type_line.Text = (string)data.SelectToken("type_line");
+            txb_oracle_text.Text = (string)data.SelectToken("oracle_text");
+            txb_mana_cost.Text = (string)data.SelectToken("mana_cost");
         }
 
         private void Test_Click(object sender, RoutedEventArgs e)
         {
-            Test_ClickAsync();
-        }
-    }
+            /*  JObject token;
+              await Task.Run(() => {
+                  token = JObject.Parse(File.ReadAllText(@"C:\Users\Joseph\Documents\Visual Studio 2017\Projects\MagicApplication\MagicApp\AllSets.json"));
+                  PopulateData(token);
+                  });
 
-}
+              }*/
+            string path = "AllSets.json";
+            byte[] byteArray = Encoding.UTF8.GetBytes(path);
+            //byte[] byteArray = Encoding.ASCII.GetBytes(contents);
+            MemoryStream stream = new MemoryStream(byteArray);
+            StreamReader sr = new StreamReader(stream);
+            string text = sr.ReadToEnd();
+            txb_Name.Text = text;
+            JObject token = JObject.Parse(File.ReadAllText(text));
+            PopulateData(token);
+            txb_cmc.Text = (string)token.SelectToken("name");
+            //Test_ClickAsync();
+
+        }
+
+    }
+    }
